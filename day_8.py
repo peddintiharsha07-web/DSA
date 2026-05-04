f26e108431b9ab3d231031b9b3a66d1bb31b7a39
Peddinti Harsha Vardhan <knowledgegainersteam20@gmail.com>
2026-05-04 22:35:35 +0530

    python dsa

--- day_8.py
+++ day_8.py
@@ -7,3 +7,13 @@ for i in range(k , len(arr)):
     win += arr[i] - arr[i-k]
     s = max(win,s)
 print(s)
+
+arr = [2,3,4,5,6,7,8,9]
+k = 2
+
+win = sum(arr[0:k])
+s = win
+for i in range(k , len(arr)):
+    win += arr[i] - arr[i-k]
+    s = max(win,s)
+print(s)
